cf2aaeaa3d904e6887867319f126993132c1cb04
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-05-06 17:08:08 +0200

    setup and configure I18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: [
-    '@nuxt/test-utils/module'
-  ]
+  modules: ['@nuxt/test-utils/module', '@nuxtjs/i18n']
 })
