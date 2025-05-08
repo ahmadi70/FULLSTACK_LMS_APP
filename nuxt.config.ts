0299ac241b82a1c3ba0ffad907cbc74ba36d8b4a
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-05-08 21:01:53 +0200

    configure english locale for i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/test-utils/module', '@nuxtjs/i18n']
+  modules: ['@nuxt/test-utils/module', '@nuxtjs/i18n'],
+  i18n: {
+    defaultLocale: 'en',
+    locales: [
+      { code: 'en', name: 'English', file: 'en.json' }
+    ]
+  }
 })
