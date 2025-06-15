7a7d6f3eb2a2d43f226d8d9b3f82be0455a6e96b
Nahid Ahmadi <nahidahmadi760@gmail.com>
2025-06-15 16:53:02 +0200

    configure variables, mixins, and functions scss files

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,5 +8,18 @@ export default defineNuxtConfig({
     locales: [
       { code: 'en', name: 'English', file: 'en.json' }
     ]
-  }
+  },
+  vite:{
+    css: {
+      preprocessorOptions: {
+        scss: {
+          additionalData: `
+          @use "@/assets/style/variables.scss" as *;
+          @use "@/assets/style/mixins.scss" as *;
+          @use "@/assets/style/functions.scss" as *;
+          `,
+        },
+      },
+    },
+   }
 })
